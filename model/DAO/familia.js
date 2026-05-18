f03432cc8a147b4f11139572ad3f7ae18550f508
KauanAntunes <kazinhg2007@gmail.com>
2026-05-17 21:58:22 -0300

    em andamento

--- model/DAO/familia.js
+++ model/DAO/familia.js
@@ -1,99 +1,116 @@
 /***********************************************
- * Objetivo: Arquivo de responsavel pela realização do CRUD no banco de dados SQL
- * Autor: Gustavo de Paula Silva
- * Data: 22/04/2026
- * Versão: 1.0
+ * Objetivo: Arquivo responsavel pelo CRUD de família
  ************************************************/
-const knex = require("knex");
-const knexConfig = require("../database_config/knexfile");
 
-const knexDatabase = knex(knexConfig.development);
+const knex = require("knex")
+const knexConfig = require("../database_config/knexfile")
 
-//GET  
-const getAllFamilys = async function(){
-    try{
-        let sql = "select * from tb_familia"
+const knexDatabase = knex(knexConfig.development)
+
+// GET
+const getAllFamilys = async function () {
+    try {
+
+        let sql = `select * from tb_familia`
+
         let result = await knexDatabase.raw(sql)
-        if(Array.isArray(result)){
-            return result
-        }else{
-            return false
-        }
-    }catch(error){
+
+        return result[0]
+
+    } catch (error) {
+
         console.log(error)
-        return error
-    }   
+
+        return false
+    }
 }
-//GET por id
-const getFamilyById = async function(id) {
-    try{
+
+// GET BY ID
+const getFamilyById = async function (id) {
+    try {
+
         let sql = `select * from tb_familia where id_familia = ${id}`
+
         let result = await knexDatabase.raw(sql)
-        if(Array.isArray(result)){
-            return result
-        }else{
-            return false
-        }
-    }catch(error){
+
+        return result[0]
+
+    } catch (error) {
+
         console.log(error)
-        return error
-    }   
+
+        return false
+    }
 }
-//POST
-const setInsertFamily = async function(familia) {
-    try{
-        let sql = `insert into tb_familia(
-                        nome,
-                        telefone_residencial
-                    )values(
-                        '${familia.nome}',
-                        '${familia.telefone_residencial}'
-                    )`
+
+// POST
+const setInsertFamily = async function (familia) {
+    try {
+
+        let sql = `
+            insert into tb_familia (
+                nome,
+                telefone_residencial
+            )
+            values (
+                '${familia.nome}',
+                '${familia.telefone_residencial}'
+            )
+        `
+
         let result = await knexDatabase.raw(sql)
-        if(Array.isArray(result)){
-            return result
-        }else{
-            return false
-        }
-    }catch(error){
+
+        return !!result
+
+    } catch (error) {
+
         console.log(error)
-        return error
-    }   
+
+        return false
+    }
 }
-//PUT
-const setUpdateFamily = async function(familia) {
-    try{
-        let sql = `update tb_familia set
-                        nome = '${familia.nome}',
-                        telefone_residencial = '${familia.telefone_residencial}'
-                    where id_familia = ${familia.id_familia}
-                    )`
+
+// PUT
+const setUpdateFamily = async function (familia) {
+    try {
+
+        let sql = `
+            update tb_familia set
+                nome = '${familia.nome}',
+                telefone_residencial = '${familia.telefone_residencial}'
+            where id_familia = ${familia.id_familia}
+        `
+
         let result = await knexDatabase.raw(sql)
-        if(Array.isArray(result)){
-            return result
-        }else{
-            return false
-        }
-    }catch(error){
+
+        return !!result
+
+    } catch (error) {
+
         console.log(error)
-        return error
-    }   
+
+        return false
+    }
 }
-//DELETE
-const setDeleteFamily = async function(id) {
-    try{
+
+// DELETE
+const setDeleteFamily = async function (id) {
+    try {
+
         let sql = `delete from tb_familia where id_familia = ${id}`
+
         let result = await knexDatabase.raw(sql)
-        if(Array.isArray(result)){
-            return result
-        }else{
-            return false
-        }
-    }catch(error){
+
+        return !!result
+
+    } catch (error) {
+
         console.log(error)
-        return error
-    }   
+
+        return false
+    }
 }
+
 module.exports = {
     getAllFamilys,
     getFamilyById,
